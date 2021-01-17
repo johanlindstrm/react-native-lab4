d07a1a67956d39682741534d9f4e577875170f7c
Johan <johanlindstroom@icloud.com>
2021-01-17 21:53:05 +0100

    Cleaning up and adding some documentation

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, SafeAreaView, StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Button } from 'react-native';
 
-// Load Feed Button
-
-// Youtube Feed From Youtube API
-  // Save and show it in ListView
-  // Thumbnail, Title, Channel
-
-//let url = 'https://jsonplaceholder.typicode.com/posts'
-
-let url = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=AIzaSyC0U9QWdWNITVbiO5NrgnkKPqMc1rxt4eI'
-
 const dummyData = [
   {id:1, channel:'youtuber', title:'First Title', url:require('./assets/icon.png')},
   {id:2, channel:'youtuber', title:'Second Title', url:require('./assets/icon.png')},
@@ -21,98 +11,6 @@ const dummyData = [
   {id:7, channel:'youtuber', title:'Seventh Title', url:require('./assets/icon.png')},
 ]
 
-
-const search = require("./assets/search.png")
-const close = require("./assets/close.png")
-const confirm = require("./assets/confirm.png")
-
-const Header = () => {
-  const [searching, setSearching] = useState(false);
-  return (
-  <SafeAreaView style={{backgroundColor:'white'}}>  
-
-    {searching ?
-    <SearchHeader setSearching={setSearching} searching={searching} /> 
-    : <DefaultHeader setSearching={setSearching} searching={searching}/>
-    }
-
-   {/* <DefaultHeader />*/}
-   {/* <SearchHeader />*/}
-  </SafeAreaView>
-  );
-};
-
-
-const DefaultHeader = ({setSearching, searching}) => {
-  return (
-    <View style={{alignItems:'center', height: 50, flexDirection:'row', justifyContent:'center'}}>
-
-    <View style={{flex:1}}>
-      <Text style={{fontSize:20, fontWeight:'bold', paddingLeft:20, alignSelf:'flex-start'}}>
-        YouTube
-      </Text>
-    </View>
-    
-    <View style={{flex:1}}>
-      <TouchableOpacity style={{marginRight:20, alignSelf:'flex-end'}} onPress={() => {setSearching(!searching);}}> 
-        <Image style={{width:30, height:30}} source={search} />
-      </TouchableOpacity>
-    </View>
-
-  </View>
-  )
-}
-
-const SearchHeader = ({listData, setListData, setSearching, searching}) => {
-  // Todo
-    // TextInput
-    // Handle the search text
-    // Track input from the user
-    // Filter through the list of data
-/*  const [query, setQuery] = useState('');
-
-  const handleSearch = text => {
-    const formattedQuery = text.toLowerCase();
-    const filteredData = filter(listData, video => {
-      return contains(video, formattedQuery);
-    });
-    setData(filteredData);
-    setQuery(text);
-  };
-  */
-
-  return(
-    <View style={{alignItems:'center', height: 50, flexDirection:'row', justifyContent:'center'}}>
-      
-      <View style={{flex:1}}>
-        <TouchableOpacity style={{marginLeft:20, alignSelf:'flex-start'}} onPress={() => {setSearching(!searching);}}> 
-          <Image style={{width:15, height:15}} source={close} />
-        </TouchableOpacity>
-      </View>
-
-      <View style={{flex:1}}>
-        <TextInput
-          autoCapitalize="none"
-          autoCorrect={false}
-          clearButtonMode="always"
-          //value={query}
-          //onChangeText={queryText => handleSearch(queryText)}
-          placeholder="Search"
-          style={{ backgroundColor: '#fff', paddingHorizontal: 10 }}
-          />
-      </View>
-
-      <View style={{flex:1}}>
-        <TouchableOpacity style={{marginRight:20, alignSelf:'flex-end'}} onPress={() => console.log('search was pressed') }> 
-          <Image style={{width:30, height:30}} source={confirm} />
-        </TouchableOpacity>
-      </View>
-
-    </View>
-  )
-}
-
-
 const DummyListExample = () => {
   return (
     <View>
@@ -121,7 +19,7 @@ const DummyListExample = () => {
         data={dummyData}
         renderItem={({ item }) => {
           return (
-            <View style={styles.videoView}>
+            <View>
                 <View style={{ backgroundColor: '#529FF3', margin:10 ,width:'100%'}}>
                 <Image
                     source={item.url}
@@ -143,19 +41,104 @@ const DummyListExample = () => {
   )
 }
 
-const YoutubeListFeed = () => {
-  const [listData, setListData] = useState([]);
+let url = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=AIzaSyC0U9QWdWNITVbiO5NrgnkKPqMc1rxt4eI'
+
+const search = require("./assets/search.png")
+const close = require("./assets/close.png")
+const confirm = require("./assets/confirm.png")
+
+const Header = ({setFilterKeyWord}) => {
+  const [searching, setSearching] = useState(false);
+  return (
+  <SafeAreaView>  
+    {searching ? <SearchHeader setSearching={setSearching} searching={searching} setFilterKeyWord={setFilterKeyWord} /> 
+    : <DefaultHeader setSearching={setSearching} searching={searching}/>}
+  </SafeAreaView>
+  );
+};
+
+const DefaultHeader = ({setSearching, searching}) => {
+  return (
+    <View style={styles.headerView}>
+
+    <View style={{flex:1}}>
+      <Text style={{...styles.rightIconContainer, fontSize:20, fontWeight:'bold'}}>
+        YouTube
+      </Text>
+    </View>
+    
+    <View style={{flex:1}}>
+      <TouchableOpacity style={styles.leftIconContainer} onPress={() => {setSearching(!searching);}}> 
+        <Image style={{width:30, height:30}} source={search} />
+      </TouchableOpacity>
+    </View>
+
+  </View>
+  )
+}
+
+const SearchHeader = ({setSearching, searching, setFilterKeyWord}) => {
+
+  let keyword = ""
+
+  const handleChangeText = (value) => {
+    keyword = value
+  }
+  
+  const filterData = () => {
+    setFilterKeyWord(keyword);
+  };
+
+  const resetFilterData = () => {
+    setFilterKeyWord("");
+  }
+
+  return(
+    <View style={styles.headerView}>
+      
+      <View style={{flex:1}}>
+        <TouchableOpacity style={styles.rightIconContainer} onPress={() => {{setSearching(!searching); resetFilterData()}}}> 
+          <Image style={{width:15, height:15}} source={close} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={{flex:1}}>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          clearButtonMode="always"
+          onChangeText={handleChangeText}
+          placeholder="Search"
+          style={styles.inputStyle}
+          />
+      </View>
+
+      <View style={{flex:1}}>
+        <TouchableOpacity style={styles.leftIconContainer} onPress={() => {filterData()}}> 
+          <Image style={{width:30, height:30}} source={confirm} />
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  )
+}
+
+const YoutubeListFeed = ({listData, setListData, filterKeyWord}) => {
   const [isVisible, setIsVisible] = useState(true)
 
     const fetchData = () => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
+      fetch(url) // Fetch data from a url
+        .then((response) => response.json()) // Recieve a response and convert it in json format
+        .then((json) => { // Read the respone 
+          // Handle the response data recieved from the fetch
           let dataResults = [];
           json.items.forEach((item) => {
+            // Loop through the data getting titles, channels and thumbnails
             let result = { title: item.snippet.title, channel: item.snippet.channelTitle, imageUrl: item.snippet.thumbnails.standard.url };
+            // Push the results with {title: *, channel: *, url: *} to our array then..
             dataResults.push(result);
           });
+            // ..update the useState function for setting our listData with the results pushed earlier
           setListData(dataResults);
         });
     };
@@ -168,14 +151,13 @@ const YoutubeListFeed = () => {
       }}/>) : null}
 
       <FlatList
-        style={{width:'100%'}}
-        data={listData}
+        data={listData.filter((item) => item.title.includes(filterKeyWord))}
         renderItem={({ item }) => {
           return (
             <View style={{width:'100%', paddingBottom:10}}>
               <Image source={{uri: item.imageUrl}} style={{ height: 240, width: '100%', marginBottom:10 }} />
-              <Text style={{paddingLeft: 5, fontWeight:'bold'}} >{item.title}</Text>
-              <Text style={{paddingLeft: 5, color:'#808080'}} >{item.channel}</Text>
+              <Text style={styles.titleText} >{item.title}</Text>
+              <Text style={styles.channelText} >{item.channel}</Text>
             </View>
           );
         }}
@@ -187,11 +169,13 @@ const YoutubeListFeed = () => {
 }
 
 export default function App() {
+  const [listData, setListData] = useState([]);
+  const [filterKeyWord, setFilterKeyWord] = useState(""); 
 
-  return (
+  return ( // passing 
     <View style={styles.container}>
-      <Header />
-      <YoutubeListFeed />
+      <Header listData={listData} setListData={setListData} setFilterKeyWord={setFilterKeyWord} />
+      <YoutubeListFeed listData={listData} setListData={setListData} filterKeyWord={filterKeyWord}  />
     </View>
   );
 }
@@ -204,10 +188,38 @@ const styles = StyleSheet.create({
     height:'100%',
   },
 
-  videoView: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: "flex-start",
-    marginBottom: 25,
+  headerView: {
+    alignItems:'center', 
+    height: 50, 
+    flexDirection:'row', 
+    justifyContent:'center'
+  },
+
+  channelText: {
+    paddingLeft: 5,
+    color:'#808080',
+
+  },
+
+  titleText: {
+    paddingLeft: 5, 
+    fontWeight:'bold',
+  },
+
+  inputStyle: {
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: 10, 
+    paddingVertical:10, 
+    borderRadius:5
+  },
+
+  rightIconContainer: {
+    paddingLeft:20, 
+    alignSelf:'flex-start'
+  },
+
+  leftIconContainer: {
+    marginRight:20, 
+    alignSelf:'flex-end'
   },
 });
